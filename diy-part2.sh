2584bc7915cebb3bbc58602fca7fc008376093f1
lalblc <90815935+lalblc@users.noreply.github.com>
2021-09-16 23:36:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-ZZZ="包/精益/默认设置/文件/z-默认设置" 
-sed - i  '/ Cyxluq4wuazhjmccq / d'  $ZZZ
-sed - i  "s / Openwrt / lin007 编译在$（TZ= UTC - 8 日期"+%Y. %m.d）  [ 开放侵权 /g]  $ZZZ
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+sed -i "s/OpenWrt /lin007 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
